8ea6b1697aedc188b449c475d1222dfd8204df05
Geoff Lambert <admin@vulpineinteractive.com>
2019-02-09 03:54:07 -0700

    Updates

---
+++ precache-manifest.0ad578550475606c5ad20567dd15a174.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e766523559aa4f34baa0",
+    "url": "/triple-triad-memory-game/static/js/runtime~main.e7665235.js"
+  },
+  {
+    "revision": "1404046abd998f6d2e6b",
+    "url": "/triple-triad-memory-game/static/js/main.1404046a.chunk.js"
+  },
+  {
+    "revision": "e0b9be432fd036f232e8",
+    "url": "/triple-triad-memory-game/static/js/1.e0b9be43.chunk.js"
+  },
+  {
+    "revision": "1404046abd998f6d2e6b",
+    "url": "/triple-triad-memory-game/static/css/main.0bc8cfa9.chunk.css"
+  },
+  {
+    "revision": "7c219dae9b35437ce54f44279ebaf3cf",
+    "url": "/triple-triad-memory-game/index.html"
+  }
+];
